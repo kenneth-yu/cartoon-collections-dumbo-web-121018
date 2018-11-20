d111bb4081ef396a0c8484db0ab41d279d25722b
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 23:27:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,10 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.each do |x|
+    x.length
 end
 
 def find_the_cheese# code an argument here
