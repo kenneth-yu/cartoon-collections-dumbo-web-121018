553b0cb73a85f35224978d8f6f3ac9409de612bb
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 23:31:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,9 @@ def long_planeteer_calls(array)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(string)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include(cheese_types)
+    return 
 end
