5961238d05d9d0a74ffc4f27a36cadb05bec6a5a
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 23:20:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ require 'pry'
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |item,index|
-    puts "#{index}. #{item}"
+    puts "#{index+1}. #{item}"
   end
 end
 
