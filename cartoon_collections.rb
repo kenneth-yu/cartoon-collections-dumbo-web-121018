e8008617749a8175d2ca9db97e81f7a7086c77d7
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 23:23:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.collect do x 
     x.capitalize
     x = x + '!'
+  end
 end
 
 def long_planeteer_calls# code an argument here
